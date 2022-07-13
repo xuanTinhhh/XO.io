af3ea31d04f91afdff284db163d1e384b911656b
xuanTinhhh <xunatinhfff@gmail.com>
2022-07-13 16:21:35 +0700

    submit

---
+++ main.js
@@ -0,0 +1,92 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+// Kiểu chiến thắng
+let winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [2, 5, 8],
+    [6, 7, 8],
+    [3, 4, 5],
+    [1, 4, 7],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+let xTurn = true;
+let count = 0;
+// Vô hiệu hóa nút
+const disableButtons = () => {
+    btnRef.forEach((element) => (element.disabled = true));
+    popupRef.classList.remove("hide");
+};
+// Cho phép các nút
+const enableButtons = () => {
+    btnRef.forEach((element) => {
+        element.innerText = "";
+        element.disabled = false;
+    });
+    popupRef.classList.add("hide");
+};
+// Xử lý sự kiện khi click vào new game
+newgameBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+});
+// Xử lý sự kiện khi click vào restart
+restartBtn.addEventListener("click", () => {
+    count = 0;
+    enableButtons();
+});
+// Người chiến thắng
+const winFunction = (letter) => {
+    disableButtons();
+    if (letter == "X"){
+        msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+    }else{
+        msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+    }
+};
+// Hòa..
+const drawFunction = () => {
+    disableButtons();
+    msgRef.innerHTML = "&#x1F60E; <br> Hòa";
+}
+// Kiểm tra người chiến thắng
+const winChecker = () => {
+    for (let i  of winningPattern) {
+        let [element1, element2, element3] = [
+            btnRef[i[0]].innerText, 
+            btnRef[i[1]].innerText, 
+            btnRef[i[2]].innerText,
+        ];
+        if(element1 != "" & (element2 != "") && (element3 != "")){
+            if(element1 == element2 && element2 == element3) {
+                winFunction(element1);
+            }
+        }
+    }
+}
+// X - O
+btnRef.forEach((element) => {
+    element.addEventListener("click", () => {
+        if(xTurn){
+            xTurn = false;
+            element.innerText = "X";
+            element.disabled = true;
+        }else{
+            xTurn = true;
+            element.innerText = "O";
+            element.disabled = true;
+        }
+        count += 1;
+        if(count === 9){
+            drawFunction();
+        }
+        winChecker();
+    })
+});
+
+window.onload = enableButtons;
